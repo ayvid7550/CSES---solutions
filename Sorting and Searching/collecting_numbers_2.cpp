88d78afca8af9519c83333a1768511e9c17223d6
ayvid7550 <pro.grammer4542@gmail.com>
2026-08-12 19:10:36 +0530

    stuff

--- Sorting and Searching/collecting_numbers_2.cpp
+++ Sorting and Searching/collecting_numbers_2.cpp
@@ -1,41 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define fast ios::sync_with_stdio(false); cin.tie(NULL);
-
 int main() {
-    fast;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n, m;
-    cin>>n>>m;
-    int ans = 1;
-    vector <int> v (n);
-    vector <int> pos (n+1);
-    for(int i = 0; i < n; i++) {
-        cin>>v[i];
-        pos[v[i]] = i;
-    }
-    for(int i = 0; i < n; i++) {
-        if(pos[v[i]] < pos[v[i] + 1]) {
-            ans++;
-        }
-        pos[v[i]];
+    cin >> n >> m;
+
+    vector<int> a(n + 1);
+    vector<int> pos(n + 1);
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        pos[a[i]] = i;
     }
 
-    for(int i = 0; i < m; i++) {
-        ans = ans - 2;
-        int a , b;
-        a--;
-        b--;
-        cin>>a>>b;
-        swap(v[a], v[b]);
-        if(pos[v[a] - 1] < pos[v[a]]) {
-            ans++;
-        }
-        if(pos[v[a]] < pos[v[a]+1]) {
-            ans++;
-        }
-        cout<<ans<<"\n";
+    int breaks = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (pos[i] > pos[i + 1])
+            breaks++;
+    }
+
+    auto bad = [&](int x) -> int {
+        if (x < 1 || x >= n)
+            return 0;
+        return pos[x] > pos[x + 1];
+    };
+
+    while (m--) {
+        int x, y;
+        cin >> x >> y;
+
+        int vx = a[x];
+        int vy = a[y];
+
+        set<int> affected = {
+            vx - 1, vx,
+            vy - 1, vy
+        };
+
+        for (int v : affected)
+            breaks -= bad(v);
+
+        swap(a[x], a[y]);
+
+        swap(pos[vx], pos[vy]);
+
+        for (int v : affected)
+            breaks += bad(v);
+
+        cout << breaks + 1 << '\n';
     }
 
     return 0;
